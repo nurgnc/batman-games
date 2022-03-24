5f8b299f849e44f009a9df6f2b084c27af257aaa
nurgnc <gencbedriyenur@gmail.com>
2022-03-24 09:57:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,20 @@ function getBatmanGames() {
     .then((response) => response.json())
     .then((data) => {
       let html = "";
-      console.log(data);
       data.map((item, index) => {
         html += `
-                <div class = "game-item w-3/12 border border-sky-500 flex flex-col items-center justify-center p-5 m-2"  data-id = "${item.gameID}">
-                    <div class = "game-img">
-                        <img src = "${item.thumb}" alt = "batman-game">
+                <div class = "game-item w-3/12 border rounded-md  border-sky-500 flex flex-col  p-5 m-2"  data-id = "${item.gameID}">
+                    <div class = "game-img flex flex-col items-center">
+                        <img class="object-contain h-48 w-96" src = "${item.thumb}" alt = "batman-game">
                     </div>
-                    <div class = "game-name">
-                        <h3>${item.title}</h3>
-                        <h6>Deal Rating: ${item.dealRating}</h6>
+                    <div class = "game-name mt-2">
+                        <div class="flex flex-row items-center justify-between">
+                          <h3 class="text-lg font-semibold">${item.title}</h3>
+                          <div class="border text-white bg-green-400 p-2 rounded-full">${item.dealRating}</div>
+                        </div>
                         <div>
-                          <span>Normal Price: ${item.normalPrice}, </span>
-                          <span> Sale Price: ${item.salePrice}</span>
+                          <span class="font-mono font-bold">$${item.normalPrice}</span>
+                          <span class="font-mono text-sm line-through text-red-600 font-semibold ml-3">$${item.salePrice}</span>
                         </div>
                     </div>
                 </div>
